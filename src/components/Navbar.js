a1119457e393eeada4668fb766d5da53d65093b9
djoseitz <72769693+djoseitz@users.noreply.github.com>
2021-05-21 15:51:23 -0700

    initial commit of new build

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  render() {
+    const {player1, player2, ties, userName1, userName2 } = this.props;
+    return (
+      <div className="navbar">
+        <div className="title">
+          <h1>Tic Tac Go!</h1>
+        </div>
+        <div className="stats">
+          <h2>
+            {userName1}:{player1}
+          </h2>
+          <h2>
+            {userName2}:{player2}
+          </h2>
+          <h2>Ties: {ties}</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Navbar;
